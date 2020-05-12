f94aa2a678010c17629da79f8c662521d9ea05ab
18004885 <18004885@myrp.edu.sg>
2020-05-12 10:44:26 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/demoexplicitintent/Hero.java
@@ -0,0 +1,8 @@
+package sg.edu.rp.c346.demoexplicitintent;
+
+import java.io.Serializable;
+
+public class Hero implements Serializable {
+
+
+}
---
+++ app/src/main/java/sg/edu/rp/c346/demoexplicitintent/MainActivity.java
@@ -0,0 +1,23 @@
+package sg.edu.rp.c346.demoexplicitintent;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
